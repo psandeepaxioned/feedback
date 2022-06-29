160a24ac28af3a17741ba46aabe39289de3abc18
psandeepaxioned <vikasb@axioned.com>
2022-06-29 19:43:37 +0530

    working on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,31 @@
 /* Author: 
 
 */
-const sendreview = document.querySelector('a')
+const modes = document.querySelectorAll('.moods li')
 
-sendreview.addEventListener('submit',function(event){
-    
-    console.log('sendreview')
+
+
+console.log(modes);
+modes.forEach((item, idx) => {
+    item.addEventListener('click', () => {
+        removeclassactive()
+        
+        item.classList.add('active')
+        const contents = document.querySelector('.active').textContent;
+        console.log(contents)
+
+        const sendreview = document.querySelector('.send-review a')
+        console.log(sendreview)
+        sendreview.addEventListener('click',() =>{
+                feedbacksection()
+            })
+        sendreview.forEach(sendreview => sendreview.classList.add(section.feedback))
+
+    })
 })
 
-function openfeedback() {
-    sendreview.classList.add('')
+function removeclassactive(){
+    modes.forEach(moods => moods.classList.remove('active'))
 }
 
 
