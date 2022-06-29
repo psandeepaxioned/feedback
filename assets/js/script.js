db330d7aeb7ad0ae565259ad8c6ed8dfc8ab9c8a
psandeepaxioned <vikasb@axioned.com>
2022-06-29 18:01:29 +0530

    working on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
 /* Author: 
 
 */
+const sendreview = document.querySelector('a')
 
+sendreview.addEventListener('submit',function(event){
+    
+    console.log('sendreview')
+})
 
+function openfeedback() {
+    sendreview.classList.add('')
+}
 
 
 
