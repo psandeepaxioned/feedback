803029f4fc69fe1df3dbadedf16e29fbfd84f1b1
psandeepaxioned <vikasb@axioned.com>
2022-06-30 10:49:16 +0530

    working on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,32 @@
 /* Author: 
 
 */
-const modes = document.querySelectorAll('.moods li')
-
-
+const modes = document.querySelectorAll(".moods li");
 
 console.log(modes);
 modes.forEach((item, idx) => {
-    item.addEventListener('click', () => {
-        removeclassactive()
-        
-        item.classList.add('active')
-        const contents = document.querySelector('.active').textContent;
-        console.log(contents)
+  item.addEventListener("click", () => {
+    removeclassactive();
 
-        const sendreview = document.querySelector('.send-review a')
-        console.log(sendreview)
-        sendreview.addEventListener('click',() =>{
-                feedbacksection()
-            })
-        sendreview.forEach(sendreview => sendreview.classList.add(section.feedback))
+    item.classList.add("active");
+    const contents = document.querySelector(".active").textContent;
+    console.log(contents);
 
-    })
-})
+    const sendreview = document.querySelector(".send-review a");
+    console.log(sendreview);
+    sendreview.addEventListener("click", () => {
+      document.querySelector(".review-received").innerHTML =
+        "feedback: " + contents;
+      document.querySelector(".feedback").style.display = "block";
+      // feedbacksection();
+    });
+  });
+});
+// function feedbacksection(){
 
-function removeclassactive(){
-    modes.forEach(moods => moods.classList.remove('active'))
+//     sendreview.classList.add(section.feedback)
+// }
+
+function removeclassactive() {
+  modes.forEach((moods) => moods.classList.remove("active"));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
